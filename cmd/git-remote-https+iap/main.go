ca4b689f52619f052931b8b04eb839c0fe4689aa
Stephane Tang <hi@stang.sh>
2020-03-22 22:25:27 +0000

    handle browser authentication workflow

--- cmd/git-remote-https+iap/main.go
+++ cmd/git-remote-https+iap/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	_url "net/url"
 	"os"
 
+	"github.com/adohkan/git-remote-https-iap/internal/iap"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
@@ -18,10 +20,7 @@ var (
 		Use:   fmt.Sprintf("%s remote url", BinaryName),
 		Short: "git-remote-helper that handles authentication for GCP Identity Aware Proxy",
 		Args:  cobra.ExactArgs(2),
-		Run: func(cmd *cobra.Command, args []string) {
-			remote, url := args[0], args[1]
-			log.Debug().Msgf("%s %s %s", BinaryName, remote, url)
-		},
+		Run:   execute,
 	}
 )
 
@@ -35,4 +34,34 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func execute(cmd *cobra.Command, args []string) {
+	remote, url := args[0], args[1]
+	log.Debug().Msgf("%s %s %s", BinaryName, remote, url)
+
+	handleIAPAuthCookieFor(url)
+}
+
+func handleIAPAuthCookieFor(url string) {
+	// All our work will be based on the basedomain of the provided URL
+	// as IAP would be setup for the whole domain.
+	url, err := toHTTPSBaseDomain(url)
+	if err != nil {
+		log.Error().Msgf("Could not convert %s in https://: %s", url, err)
+	}
+
+	log.Debug().Msgf("Manage IAP auth for %s", os.Args[0], url)
+
+	if _, err = iap.NewCookie(url); err != nil {
+		log.Fatal().Msg(err.Error())
+	}
+}
+
+func toHTTPSBaseDomain(addr string) (string, error) {
+	u, err := _url.Parse(addr)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("https://%s", u.Host), nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module github.com/adohkan/git-remote-https-iap
 go 1.13
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/int128/oauth2cli v1.10.0
+	github.com/pkg/browser v0.0.0-20180916011732-0a3d74bf9ce4
 	github.com/rs/zerolog v1.18.0
 	github.com/spf13/cobra v0.0.6
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
 )
